12f9ca4c99b53f10936fc4db91a99b34a50366f6
zorlex <83305816+zorlex25@users.noreply.github.com>
2025-06-21 16:46:02 +0200

    Create chillout.user.js

---
+++ chillout.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Chillout Script
+// @namespace    https://github.com/zorlex25/LSS
+// @version      2.3
+// @description  Chillout
+// @author       zorlex25
+// @match        *://www.leitstellenspiel.de/*
+// @grant        GM_addStyle
+// @require      https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js
+// @updateURL    https://raw.githubusercontent.com/zorlex25/lss/main/chillout.user.js
+// @downloadURL  https://raw.githubusercontent.com/zorlex25/lss/main/chillout.user.js
+// ==/UserScript==
